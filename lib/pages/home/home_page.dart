1f008114e5d635a868ac9cbf4f5e792a4f06b387
Zaero Blitz <zaeroblitz@gmail.com>
2021-07-10 16:49:09 +0700

    Add Header Homepage

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -2,13 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
 
 class HomePage extends StatelessWidget {
+  Widget header() {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 30,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hello, Zaero',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: semiBold,
+                  ),
+                ),
+                Text(
+                  '@zaeroblitz',
+                  style: subtitleTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: semiBold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: 54,
+            height: 54,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage('assets/image_profile.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Homepage',
-        style: secondaryTextStyle,
-      ),
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
