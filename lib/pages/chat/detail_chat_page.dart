f4c5178d58dc85de46b3e7679145b776d9f7a3c4
Zaero Blitz <zaeroblitz@gmail.com>
2021-07-12 11:21:38 +0700

    Add ChatInput

--- lib/pages/chat/detail_chat_page.dart
+++ lib/pages/chat/detail_chat_page.dart
@@ -50,11 +50,49 @@ class DetailChatPage extends StatelessWidget {
     );
   }
 
+  Widget chatInput() {
+    return Container(
+      height: 45,
+      margin: EdgeInsets.all(24),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                color: backgroundColor4,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Center(
+                child: TextFormField(
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'Type message...',
+                    hintStyle: subtitleTextStyle,
+                  ),
+                  style: primaryTextStyle,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Image.asset(
+            'assets/send_button.png',
+            width: 45,
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor3,
       appBar: appBar(),
+      bottomNavigationBar: chatInput(),
     );
   }
 }
