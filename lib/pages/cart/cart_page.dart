96fb93dadb5d5a3d6ad1d81d386a5f31ccd21770
Zaero Blitz <zaeroblitz@gmail.com>
2021-07-14 15:11:39 +0700

    Add CartPage EmptySection

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -67,12 +67,67 @@ class CartPage extends StatelessWidget {
       );
     }
 
+    Widget emptyCart() {
+      return Center(
+        child: Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/shopping_cart_icon.png',
+                width: 80,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Opss! Your Cart is Empty',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                "Let's find your favorite shoes",
+                style: subtitleTextStyle,
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                      backgroundColor: primaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 12,
+                      )),
+                  child: Text(
+                    'Explore Store',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: medium,
+                    ),
+                  ))
+            ],
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor3,
       appBar: appBar(),
-      body: Center(
-        child: Text('Cart Page'),
-      ),
+      body: emptyCart(),
     );
   }
 }
